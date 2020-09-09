0e8b0be0a9ace3919093491170d2f303b9926f3a
Yogesh Deshmukh <sampoorna.mahanti@tcs.com>
2020-09-09 08:13:26 +0530

    Initial Commit

---
+++ src/main/java/com/hibernate/demo/HibernateJpaDemoApplication.java
@@ -0,0 +1,13 @@
+package com.hibernate.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HibernateJpaDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HibernateJpaDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hibernate/demo/HibernateJpaDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hibernate.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HibernateJpaDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
